26268700567478754b459314e96e0846023ce294
Ashiqur Rahman Tusher <tusher422422@gmail.com>
2025-08-18 11:21:52 +0600

    learn with tawhid

---
+++ CODES/src/LWT.java
@@ -0,0 +1,21 @@
+//lecture 16 ,, multi class
+
+public class LWT {
+    public static void main(String[] args){
+
+        student s1 = new student("rahi","011251");
+        student s2 = new student("tusher","011243");
+
+        course c1 = new course("cse 1032", "Object oriented programing");
+        course c2 = new course("cse 1325","Digital logic Design");
+
+        s1.addcourse(c1);
+        s1.addcourse(c2);
+        s1.showdetail();
+        s2.addcourse(c2);
+        s2.showdetail();
+
+
+
+    }
+}
---
+++ CODES/src/student.java
@@ -0,0 +1,30 @@
+// lec 16 under multi class
+import java.util.Arrays;
+public class student {
+    public String name;
+    public String id;
+    public course[] courses=new course[5];
+    public int coursecount = 0;
+    public student(String nm,String id){
+        this.name = nm;
+        this.id = id;
+    }
+
+    public void addcourse(course c){
+        courses[coursecount]=c;
+        coursecount++;
+    }
+
+    public void showdetail(){
+        System.out.println("Name : "+name);
+        System.out.println("Id : "+id);
+        System.out.println("Courses Taken");
+        for (int i =0;i<coursecount;i++){
+            System.out.println(i+1);
+            System.out.println(courses[i].code);
+            System.out.println(courses[i].title);
+        }
+
+    }
+
+}
